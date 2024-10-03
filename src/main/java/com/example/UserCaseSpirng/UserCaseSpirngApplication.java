bbe0210ac1b0f6c6d849711bade33591448f07f5
joaopfmonteiro <joaoosverdes@gmail.com>
2024-10-03 12:24:25 +0100

    "This is a first commit"

---
+++ src/main/java/com/example/UserCaseSpirng/UserCaseSpirngApplication.java
@@ -0,0 +1,13 @@
+package com.example.UserCaseSpirng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCaseSpirngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserCaseSpirngApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/UserCaseSpirng/UserCaseSpirngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UserCaseSpirng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCaseSpirngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
